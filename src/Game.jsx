ef3f1a5e5976bc436ab00ec3d5a636ed09011b1d
LevelUpTutorial <kim_michael_jansen@yahoo.de>
2025-01-20 21:48:09 +0100

    Update Game.jsx

--- src/Game.jsx
+++ src/Game.jsx
@@ -209,7 +209,7 @@ const handleActiveAttack = () => {
                 <div className="col-6">
                   <ListHeroStats />
                 </div>
-                <div className="col-6 d-flex flex-column gap-2" role="group" aria-label="buttons for upgrade choices">
+                <div className="col-6 btn-group-vertical d-flex flex-column gap-1" role="group" aria-label="buttons for upgrade choices">
                   {upgrades.map((upgrade, index) => (
                     <button
                       key={index}
