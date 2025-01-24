c66738d9e64263e2e3c429e2bf66f01adb7bdaa7
LevelUpTutorial <kim_michael_jansen@yahoo.de>
2025-01-24 15:42:51 +0100

    Update AttackCooldownWithAnimation.jsx

--- src/AttackCooldownWithAnimation.jsx
+++ src/AttackCooldownWithAnimation.jsx
@@ -30,7 +30,7 @@ const AttackCooldownWithAnimation = ({ gameState, timingWindowStart, timingWindo
   const timingWindowWidth = widthTimingWindowEnd - widthTimingWindowStart;
 
   return (
-    <div style={{ position: 'relative', height: '20px' }}>
+    <div style={{ position: 'relative', height: '30px' }}>
       <style>{pulseAnimation}</style>
 
       {/* Main cooldown progress bar */}
