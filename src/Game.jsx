6f0bdaae8b75ebd0d85aecd19b72147cc0b1270a
LevelUpTutorial <kim_michael_jansen@yahoo.de>
2025-01-28 15:59:39 +0100

    Update Game.jsx

--- src/Game.jsx
+++ src/Game.jsx
@@ -13,7 +13,7 @@ import {combatCalculation, playSound, SND_SWORD_HIT,
         UPGRADE_CRIT_DAMAGE, UPGRADE_EVADE_CHANCE, UPGRADE_ATTACK_SPEED, 
         DAMAGE_INCREMENT, MAX_HEALTH_INCREMENT, CRIT_CHANCE_INCREMENT, 
         CRIT_DAMAGE_INCREMENT, EVADE_CHANCE_INCREMENT, ATTACK_SPEED_DECREASE, 
-        saveGameState, createActiveEnemy, getEnemyLoot, 
+        saveGameState, createActiveEnemy, getEnemyLoot, getItemImage, 
        } from "./GameUtils.js";
 
 import PropTypes from "prop-types";
