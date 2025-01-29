53f21857975a680d6c6a9e6b58d717b3160a1924
LevelUpTutorial <kim_michael_jansen@yahoo.de>
2025-01-29 23:42:43 +0100

    Update LootPopup.jsx

--- src/components/LootPopup.jsx
+++ src/components/LootPopup.jsx
@@ -56,13 +56,7 @@ const LootPopup = ({ show, currentItem, newItem, onEquip, onKeep }) => {
       <div className="modal-dialog modal-lg">
         <div className="modal-content">
           <div className={`modal-header ${newRarityBgClass}`}>
-            <h5 className="modal-title">You found...</h5>
-            <button
-              type="button"
-              className="btn-close"
-              aria-label="Close"
-              onClick={onKeep}
-            ></button>
+            <h5 className="modal-title text-white">You found...</h5>
           </div>
           <div className="modal-body d-flex justify-content-around align-items-center">
             {/* Current Item */}
