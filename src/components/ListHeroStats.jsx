b1f98308d687b30afd267bb8dbbe56033240ba2d
LevelUpTutorial <kim_michael_jansen@yahoo.de>
2025-02-03 10:40:04 +0100

    Runden

--- src/components/ListHeroStats.jsx
+++ src/components/ListHeroStats.jsx
@@ -11,7 +11,7 @@ import GameState from './../GameState.js';
 const ListHeroStats = ({ gameState }) => {
   if (!gameState || !gameState.hero) return null;
 
-  const heroHPCramped = Math.round(Math.max(0, gameState.hero.health));
+  const heroHPCramped = Math.ceil(Math.max(0, gameState.hero.health));
 
   return (
     <div className="d-flex flex-column align-items-center text-center">
@@ -21,7 +21,7 @@ const ListHeroStats = ({ gameState }) => {
       </p>
       {/* Uncomment if needed */}
       {/* <p>
-        <strong>Exp:</strong> {gameState.hero.xp} / {gameState.hero.xp_to_levelup}
+        <strong>Exp:</strong> {Math.floor(gameState.hero.xp)} / {Math.ceil(gameState.hero.xp_to_levelup)}
       </p> */}
       <p>
         <strong>Health:</strong> {heroHPCramped}
@@ -30,40 +30,40 @@ const ListHeroStats = ({ gameState }) => {
           : ""}
       </p>
       <p>
-        <strong>Attack:</strong> {gameState.hero.attack}
+        <strong>Attack:</strong> {Math.floor(gameState.hero.attack)}
         {gameState.hero.upgradeCounts[UPGRADE_DAMAGE] >= GameState.UPGRADE_LIMITS[UPGRADE_DAMAGE]
           ? " (max)"
           : ""}
       </p>
       <p>
-        <strong>Attack Cooldown:</strong> {gameState.hero.attack_speed / 1000}s
+        <strong>Attack Cooldown:</strong> {Math.round(gameState.hero.attack_speed / 10) / 100}s
         {gameState.hero.upgradeCounts[UPGRADE_ATTACK_SPEED] >= GameState.UPGRADE_LIMITS[UPGRADE_ATTACK_SPEED]
           ? " (max)"
           : ""}
       </p>
       <p>
-        <strong>Evade Chance:</strong> {gameState.hero.evade_chance}%
+        <strong>Evade Chance:</strong> {Math.round(gameState.hero.evade_chance * 100) / 100}%
         {gameState.hero.upgradeCounts[UPGRADE_EVADE_CHANCE] >= GameState.UPGRADE_LIMITS[UPGRADE_EVADE_CHANCE]
           ? " (max)"
           : ""}
       </p>
       <p>
-        <strong>Damage Reduction:</strong> {gameState.hero.damage_reduction}%
+        <strong>Damage Reduction:</strong> {Math.round(gameState.hero.damage_reduction * 100) / 100}%
       </p>
       <p>
-        <strong>Crit Chance:</strong> {gameState.hero.crit_chance}%
+        <strong>Crit Chance:</strong> {Math.round(gameState.hero.crit_chance * 100) / 100}%
         {gameState.hero.upgradeCounts[UPGRADE_CRIT_CHANCE] >= GameState.UPGRADE_LIMITS[UPGRADE_CRIT_CHANCE]
           ? " (max)"
           : ""}
       </p>
       <p>
-        <strong>Crit Damage:</strong> {gameState.hero.crit_damage}%
+        <strong>Crit Damage:</strong> {Math.round(gameState.hero.crit_damage * 100) / 100}%
         {gameState.hero.upgradeCounts[UPGRADE_CRIT_DAMAGE] >= GameState.UPGRADE_LIMITS[UPGRADE_CRIT_DAMAGE]
           ? " (max)"
           : ""}
       </p>
       <p>
-        <strong>Bonus Damage:</strong> {gameState.hero.bonus_damage}%
+        <strong>Bonus Damage:</strong> {Math.round(gameState.hero.bonus_damage * 100) / 100}%
       </p>
     </div>
   );
