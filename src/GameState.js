ac00c17a53f171e4f2e78e62cf6b6d19a83b7ffe
LevelUpTutorial <kim_michael_jansen@yahoo.de>
2025-02-04 20:19:26 +0100

    Update GameState.js

--- src/GameState.js
+++ src/GameState.js
@@ -125,7 +125,7 @@ class GameState {
   static GENDER_FEMALE = 'female';
   static DEFAULT_GENDER = GameState.GENDER_MALE;
   static MAX_LEVEL = 70;
-  static XP_TO_LEVEL2 = 65; 
+  static XP_TO_LEVEL2 = 75; 
   static XP_SCALING = 1.3; 
   static UPGRADE_LIMITS = {
     [UPGRADE_DAMAGE]: 999,
